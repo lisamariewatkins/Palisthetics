85000d7f28c49a696058c4d7c4ca02e801e2474a
lisamariewatkins <lisamwatkins@ymail.com>
2016-10-04 21:00:49 -0500

    MATCHES WORK BOO YEAH

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose'),
       Schema = mongoose.Schema,
       bcrypt = require('bcrypt-nodejs'),
-      matches = require('./matches.js');
+      matches = require('./match.js');
 
 //================================
 // User Schema
