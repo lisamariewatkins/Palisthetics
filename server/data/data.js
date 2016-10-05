b5dcedc6540b80ca7de5ae5d01df6bea50be327f
Megan King <mgnking12@gmail.com>
2016-10-04 20:22:07 -0500

    :)

---
+++ server/data/data.js
@@ -0,0 +1,23 @@
+var cardsData = [{
+    title: 'A wonderful day',
+    text: '—— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— -',
+    image: 'portrait-1.jpg',
+    id: '1'
+}, {
+    title: 'My amazing journey',
+    text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ——— ———— ',
+    image: 'portrait-2.jpg',
+    id: '2'
+}, {
+    title: 'Three recipes without cocoa',
+    text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ———',
+    image: 'portrait-3.jpg',
+    id: '3'
+}, {
+    title: 'Generic clickbait title',
+    text: ' —— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— - ——— ',
+    image: 'portrait-4.jpg',
+    id: '4'
+}];
+
+module.exports = cardsData;
